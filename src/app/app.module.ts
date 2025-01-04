b72829c0103cfa352a0295f3c0e447dd398a4fad
Braylin20 <braylin_vasquez@ucne.edu.do>
2025-01-04 15:35:34 -0400

    Configure PrimeNG

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {appConfig} from './app.config';
+import {Button} from "primeng/button";
+import { NavMenuComponent } from './shared/components/nav-menu/nav-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMenuComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        Button
+    ],
+  providers: appConfig.providers,
   bootstrap: [AppComponent]
 })
 export class AppModule { }
