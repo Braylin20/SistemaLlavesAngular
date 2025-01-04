104c2f65db0b85653b7f9f161eb112eb7eb29ff9
Braylin20 <braylin_vasquez@ucne.edu.do>
2025-01-04 16:11:10 -0400

    Add nav-menu files to build it

---
+++ src/app/shared/components/nav-menu/nav-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-nav-menu',
+  standalone: false,
+
+  templateUrl: './nav-menu.component.html',
+  styleUrl: './nav-menu.component.css'
+})
+export class NavMenuComponent {
+
+}
