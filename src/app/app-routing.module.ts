2118f5972ac6047c0602d42537ca6eb68b98f0d8
Braylin20 <braylin_vasquez@ucne.edu.do>
2025-01-12 21:49:53 -0400

    Add product register route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {DashboardComponent} from './llaves/pages/dashboard/dashboard.component';
 import {InventarioComponent} from './llaves/pages/inventario/inventario.component';
+import {RegistroProductoComponent} from './llaves/pages/registro-producto/registro-producto.component';
+import {LlavesModule} from './llaves/llaves.module';
 
 const routes: Routes = [
   {
@@ -13,13 +15,19 @@ const routes: Routes = [
     component: InventarioComponent
   },
   {
+    path: 'registro-producto',
+    component: RegistroProductoComponent
+  },
+  {
     path: '**',
     redirectTo: 'dashboard'
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
