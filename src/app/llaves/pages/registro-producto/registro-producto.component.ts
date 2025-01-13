4b33d80a0ce7081bd63b94b0efbf572a9c8b5574
Braylin20 <braylin_vasquez@ucne.edu.do>
2025-01-13 10:53:42 -0400

    Add constructor, ngOnInit, getCategorias y getProveedores

--- src/app/llaves/pages/registro-producto/registro-producto.component.ts
+++ src/app/llaves/pages/registro-producto/registro-producto.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProductoService} from '../../services/producto.service';
+import {Categorias} from '../../Interfaces/categorias';
+import {Subscription} from 'rxjs';
+import {Proveedores} from '../../Interfaces/proveedores';
 
 @Component({
   selector: 'llaves-registro-producto',
@@ -6,6 +10,25 @@ import { Component } from '@angular/core';
 
   templateUrl: './registro-producto.component.html',
 })
-export class RegistroProductoComponent {
+export class RegistroProductoComponent implements OnInit{
 
+  public categorias: Categorias[] = [];
+  public proveedores: Proveedores[] = [];
+
+  constructor(private productoService: ProductoService) {
+  }
+  ngOnInit() {
+    this.getCategorias();
+    this.getProveedores();
+  }
+
+  getCategorias() {
+    this.productoService.getCategorias()
+      .subscribe(categorias => this.categorias = categorias);
+  }
+
+  getProveedores() {
+    this.productoService.getProveedores()
+      .subscribe(proveedores => this.proveedores = proveedores);
+  }
 }
