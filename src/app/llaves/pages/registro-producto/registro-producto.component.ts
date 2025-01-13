cfea6b02227fdecdc09edaa2ec64683948dd9dc0
Braylin20 <braylin_vasquez@ucne.edu.do>
2025-01-12 21:48:06 -0400

    Add files to register the product

---
+++ src/app/llaves/pages/registro-producto/registro-producto.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'llaves-registro-producto',
+  standalone: false,
+
+  templateUrl: './registro-producto.component.html',
+  styleUrl: './registro-producto.component.css'
+})
+export class RegistroProductoComponent {
+
+}
