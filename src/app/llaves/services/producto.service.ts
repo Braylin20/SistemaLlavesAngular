1524817b171c38c599eda2540d7256706472155a
Braylin20 <braylin_vasquez@ucne.edu.do>
2025-01-13 10:52:09 -0400

    Add product service

--- src/app/llaves/services/producto.service.ts
+++ src/app/llaves/services/producto.service.ts
@@ -1,9 +1,28 @@
-import { Injectable } from '@angular/core';
+import {Injectable, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Categorias} from '../Interfaces/categorias';
+import {apiUrl} from '../utils/utils';
+import {Proveedores} from '../Interfaces/proveedores';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductoService {
+export class ProductoService implements OnInit {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {
+    this.getCategorias()
+  }
+
+  ngOnInit() {
+    this.getCategorias();
+  }
+
+  getCategorias(): Observable<Categorias[]> {
+     return this.httpClient.get<Categorias[]>(`${apiUrl}/categorias`)
+  }
+
+  getProveedores(): Observable<Proveedores[]> {
+    return this.httpClient.get<Proveedores[]>(`${apiUrl}/proveedores`)
+  }
 }
