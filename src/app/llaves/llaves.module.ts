416efd1ee34aff0720e52b6cdf4f3299eb7487cb
Braylin20 <braylin_vasquez@ucne.edu.do>
2025-01-16 22:16:46 -0400

    Edit import of RegistroProductoComponent

--- src/app/llaves/llaves.module.ts
+++ src/app/llaves/llaves.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {DashboardComponent} from './pages/dashboard/dashboard.component';
 import { InventarioComponent } from './pages/inventario/inventario.component';
-import { RegistroProductoComponent } from './pages/registro-producto/registro-producto.component';
+import { RegistroProductoComponent } from './pages/producto/registro/registro-producto.component';
 import {FormsModule} from "@angular/forms";
 import {Select} from 'primeng/select';
 import {InputNumber} from 'primeng/inputnumber';
