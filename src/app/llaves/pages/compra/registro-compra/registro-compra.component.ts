4649fbbaab18e2c3e94204b2fe156184a0030f16
Eliezer Terrero <eliezerterrero12@gmail.com>
2025-01-18 18:29:25 -0300

    add list of products

--- src/app/llaves/pages/compra/registro-compra/registro-compra.component.ts
+++ src/app/llaves/pages/compra/registro-compra/registro-compra.component.ts
@@ -4,6 +4,8 @@ import {Compra} from '../../../Interfaces/compra';
 import {CompraService} from '../../../services/compra.service';
 import {ProductoService} from '../../../services/producto.service';
 import {Proveedores} from '../../../Interfaces/proveedores';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {Producto} from '../../../Interfaces/producto';
 
 @Component({
   selector: 'app-registro-compra',
@@ -13,12 +15,13 @@ import {Proveedores} from '../../../Interfaces/proveedores';
   styleUrl: './registro-compra.component.css'
 })
 export class RegistroCompraComponent  implements OnInit {
+  registroForm!: FormGroup;
 
   public descripcion: string = '';
   loading: boolean = false;
   public proveedores: Proveedores[] = [];
-  public compras: Compra[] = [];
-  public proximaCompraId: number = 0;
+  public productos: Producto[] = [];
+  public proximaCompraId: string = "";
 
   load() {
     this.loading = true;
@@ -28,23 +31,39 @@ export class RegistroCompraComponent  implements OnInit {
   }
   public compra: Compra = CompraFactory.createDefault();
 
-  constructor(private readonly compraService: CompraService, private readonly productoService: ProductoService) {
+  constructor(
+    private readonly compraService: CompraService,
+    private readonly productoService: ProductoService,
+    private fb: FormBuilder
+  ) {
 
   }
   ngOnInit() {
     this.getProveedores()
     this.compra.fecha.getTime();
-    this.getCompras()
+    this.getCompras();
+    this.getProductos();
+
+    this.registroForm = this.fb.group({
+      fecha: ['', Validators.required],
+      concepto: ['', Validators.required],
+    })
   }
   public getProveedores() {
     this.productoService.getProveedores()
       .subscribe(proveedores => this.proveedores = proveedores);
   }
 
-  public getCompras(){
+  public getProductos() {
+    this.productoService.getProductos()
+      .subscribe(productos => this.productos = productos);
+  }
+
+  public getCompras() {
     this.compraService.getCompras()
       .subscribe(compras => {
-        this.proximaCompraId = (compras.length) + 1;
+        const siguienteId = compras.length + 1;
+        this.proximaCompraId = siguienteId.toString().padStart(5, '0');
       });
   }
 
