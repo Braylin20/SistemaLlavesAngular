88469c600542a65677738cdf3026668459b75ab6
Braylin20 <braylin_vasquez@ucne.edu.do>
2025-01-18 11:06:05 -0400

    Add cantidadValida method

--- src/app/llaves/pages/producto/registro/registro-producto.component.ts
+++ src/app/llaves/pages/producto/registro/registro-producto.component.ts
@@ -21,7 +21,7 @@ export class RegistroProductoComponent implements OnInit {
     precio: null,
     costo: null,
     cantidad: null,
-    itbis: null,
+    itbis: 18,
     descuento: null,
     descripcion: '',
     categoriaId: null,
@@ -66,6 +66,7 @@ export class RegistroProductoComponent implements OnInit {
 
 
   public addProducto() {
+
     if (!this.productoValido()) {
       this.classValidation ='ng-invalid ng-dirty'
       return;
@@ -119,6 +120,14 @@ export class RegistroProductoComponent implements OnInit {
       productoValido = false;
     if(!this.costoValido())
       productoValido = false;
+    if(!this.cantidadValida())
+      productoValido = false;
+    if(!this.itbisValido())
+      productoValido = false;
+    if(!this.descuentoValido())
+      productoValido = false;
+    if(!this.categoriaValida())
+      productoValido = false;
     return productoValido
   }
 
@@ -158,6 +167,55 @@ export class RegistroProductoComponent implements OnInit {
     }
     if(this.producto.costo > (this.producto.precio ?? 0)){
       this.message.errorCosto = 'Costo debe ser menor al precio'
+      return false
+    }
+    return true;
+  }
+
+  public cantidadValida(): boolean{
+    if(!this.producto.cantidad){
+      this.message.errorCantidad = 'Debe llenar este campo';
+      return false;
+    }
+    if(this.producto.cantidad < 1 || this.producto.cantidad > 100){
+      this.message.errorCantidad = 'El valor debe se mayor a 1';
+      return false;
+    }
+    return true;
+  }
+
+  public itbisValido(): boolean{
+    if(!this.producto.itbis){
+      this.message.errorItbis = 'Debe llenar este campo';
+      return false;
+    }
+    if(this.producto.itbis < 0 || this.producto.itbis > 18){
+      this.message.errorItbis = 'Debe ser válido';
+    }
+    return true;
+  }
+
+  public descuentoValido(): boolean{
+    if(!this.producto.descuento){
+      this.message.errorDescuento = 'Debe llenar este campo';
+      return false;
+    }
+    if(this.producto.descuento < 0){
+      this.message.errorDescuento = 'No puede ser menor a 0';
+      return false;
+    }
+    if(this.producto.descuento > 1000){
+      this.message.errorDescuento = 'No puede ser menor a 1000';
+      return false;
+    }
+    return true;
+  }
+
+  public categoriaValida(): boolean{
+    if(!this.producto.categoriaId){
+      console.log(this.producto.categoriaId);
+      this.message.errorCategoria = 'Debe llenar este campo';
+      return false;
     }
     return true;
   }
