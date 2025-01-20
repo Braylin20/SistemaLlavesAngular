c80fa4a632cdafc14cb38809e7d326eb865a497d
Braylin20 <braylin_vasquez@ucne.edu.do>
2025-01-20 19:27:48 -0400

    Add productoForm into de ngOnInit and some validations

--- src/app/llaves/pages/producto/registro/registro-producto.component.ts
+++ src/app/llaves/pages/producto/registro/registro-producto.component.ts
@@ -10,88 +10,102 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {ValidatorService} from '../../../validators/validator.service';
 
 @Component({
-    selector: 'llaves-producto',
-    standalone: false,
+  selector: 'llaves-producto',
+  standalone: false,
 
-    templateUrl: './registro-producto.component.html',
-    providers: [MessageService]
+  templateUrl: './registro-producto.component.html',
+  providers: [MessageService]
 })
 export class RegistroProductoComponent implements OnInit {
 
 
-    public formBuilder = inject(FormBuilder)
-    public message: MessageProducto = MessageProductoFactory.createDefault()
+  public formBuilder = inject(FormBuilder)
+  public message: MessageProducto = MessageProductoFactory.createDefault()
 
 
-    public categorias: Categorias[] = [];
-    public proveedores: Proveedores[] = [];
-    public garantias: Garantias[] = [];
-    public classValidation = ''
+  public categorias: Categorias[] = [];
+  public proveedores: Proveedores[] = [];
+  public garantias: Garantias[] = [];
+  public classValidation = 'ng-dirty ng-invalid'
 
-    constructor(
-        private readonly productoService: ProductoService,
-        private readonly messageService: MessageService,
-        private readonly validatorService: ValidatorService,
-    ) {
-    }
+  constructor(
+    private readonly productoService: ProductoService,
+    private readonly messageService: MessageService,
+    private readonly validatorService: ValidatorService,
+  ) {
+  }
 
-    public productoForm: FormGroup = this.formBuilder.group({
-        productoId: [0],
-        nombre: ['', [Validators.required, Validators.minLength(8)]],
-        precio: [0],
-        costo: [0],
-        cantidad: [0],
-        itbis: [0],
-        descuento: [0],
-        descripcion: [''],
-        categoriaId: [0],
-        proveedorId: [0],
-        garantiaId: [0],
-        loading: [0],
+  public productoForm!: FormGroup;
+
+  ngOnInit() {
+    this.getCategorias();
+    this.getProveedores();
+    this.getGarantias();
+    this.productoForm = this.formBuilder.group({
+      productoId: [0],
+      nombre: ['', [Validators.required]],
+      precio: [null, [Validators.required, Validators.max(100000)]],
+      costo: [null, [Validators.required]],
+      cantidad: [null, [Validators.required, Validators.max(100000)]],
+      itbis: [18, [ Validators.max(18)]],
+      descuento: [null,[Validators.required, Validators.max(100)]],
+      descripcion: [''],
+      categoriaId: [null,[Validators.required]],
+      proveedorId: [null,[Validators.required]],
+      garantiaId: [null,[Validators.required]],
+      loading: [null],
+    }, {
+      validators: [
+        this.validatorService.isCostoFewerPrecio('precio', 'costo')
+      ]
     })
-    ngOnInit() {
-        this.getCategorias();
-        this.getProveedores();
-        this.getGarantias();
-    }
+  }
 
-    public showSuccess() {
-        this.messageService.add({severity: 'success', summary: 'Éxito', detail: this.message.message});
-    }
+  public showSuccess() {
+    this.messageService.add({severity: 'success', summary: 'Éxito', detail: this.message.message});
+  }
 
-    public get currentProducto(): Producto {
-        return this.productoForm.value as unknown as Producto;
-    }
+  public get currentProducto(): Producto {
+    return this.productoForm.value as unknown as Producto;
+  }
 
-    public onSubmit() {
-        if (this.productoForm.invalid) return;
+  public onSubmit() {
+    if (this.productoForm.invalid) return;
 
-        this.productoService.addProducto(this.currentProducto)
-            .subscribe(() => {
-                this.message.message = 'Agregado Correctamente';
-                this.showSuccess();
-            });
+    this.productoService.addProducto(this.currentProducto)
+      .subscribe((prodcuto) => {
+        if(prodcuto){
+          this.showSuccess();
+          this.productoForm.reset()
+        }
+      });
 
-    }
+  }
 
-    public getCategorias() {
-        this.productoService.getCategorias()
-            .subscribe(categorias => this.categorias = categorias);
-    }
+  public getCategorias() {
+    this.productoService.getCategorias()
+      .subscribe(categorias => this.categorias = categorias);
+  }
 
-    public getProveedores() {
-        this.productoService.getProveedores()
-            .subscribe(proveedores => this.proveedores = proveedores);
-    }
+  public getProveedores() {
+    this.productoService.getProveedores()
+      .subscribe(proveedores => this.proveedores = proveedores);
+  }
 
-    public getGarantias() {
-        this.productoService.getGarantias()
-            .subscribe(garantias => this.garantias = garantias);
-    }
+  public getGarantias() {
+    this.productoService.getGarantias()
+      .subscribe(garantias => this.garantias = garantias);
+  }
 
+  public isNotValidField(field: string) {
+    return this.validatorService.isNotValidField(this.productoForm, field);
+  }
 
-    public isNotValidFiel(field: string){
-      return this.validatorService.isNotValidField(this.productoForm, field);
-    }
+  public hasMaxError(field: string) {
+    return this.validatorService.hasMaxError(this.productoForm, field);
+  }
 
+  public isCostoHigherPrecio(field: string) {
+    return this.productoForm.get(field)?.hasError('isHigher')
+  }
 }
