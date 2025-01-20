2ba72b48c90c546d2f0e8a2946f4bd77b8628a33
Braylin20 <braylin_vasquez@ucne.edu.do>
2025-01-20 19:28:25 -0400

    Add a method to evaluate if costo is fewer than precio

--- src/app/llaves/validators/validator.service.ts
+++ src/app/llaves/validators/validator.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {FormGroup} from '@angular/forms';
+import {FormGroup, ValidationErrors} from '@angular/forms';
 
 
 @Injectable({
@@ -9,7 +9,32 @@ export class ValidatorService {
 
 
   public isNotValidField(form: FormGroup, field: string) {
-    return form.controls[field].errors && form.controls[field].touched;
+    return form.controls[field].getError('required') && form.controls[field].touched;
   }
+
+  public hasMaxError(form: FormGroup, field: string) {
+    return form.controls[field].getError('max')
+  }
+
+  public isCostoFewerPrecio(field1: string, field2: string) {
+
+    return (form: FormGroup): ValidationErrors | null => {
+      const precio = form.get(field1)?.value;
+      const costo = form.get(field2)?.value;
+      if(costo > precio) {
+        form.get(field2)?.setErrors({isHigher: true})
+        return {isHigher: true};
+      }
+      if(!costo){
+        form.get(field2)?.setErrors({isNull: true})
+        return {isNull: true};
+      }
+
+
+      form.get(field2)?.setErrors(null)
+      return null;
+    }
+  }
+
 
 }
