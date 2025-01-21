720df119072ef9753b1f62fc4c73f076875c8abf
Braylin20 <braylin_vasquez@ucne.edu.do>
2025-01-21 18:01:23 -0400

    Add cuadre-caja component

---
+++ src/app/llaves/pages/Cuadre/cuadre-caja/cuadre-caja.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'llaves-cuadre-caja',
+  standalone: false,
+
+  templateUrl: './cuadre-caja.component.html',
+})
+export class CuadreCajaComponent {
+
+}
